b522e929edf1d25b0fb529c9bcf3cc3aa42d01bc
Juan Urani <juanuranidev@gmail.com>
2022-03-28 23:32:32 -0300

    updates

--- src/Components/Navbar/CartContent/CartContent.tsx
+++ src/Components/Navbar/CartContent/CartContent.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getFirestore, query, collection, where, getDocs } from 'firebase/firestore';
 import './CartContent.scss';
+import CartItem from './CartItem/CartItem';
 
 interface CartContentProps {
     openCart: boolean;
@@ -7,6 +9,18 @@ interface CartContentProps {
 }
 
 const CartContent = ({openCart, handleCloseCart}: CartContentProps) => {  
+  const [data, setData] = useState<any>([])
+
+  useEffect(() => {
+    const dataBase = getFirestore()
+    const queryCollection = query(collection(dataBase, 'products'), where('top_seller', '==', true))
+    getDocs(queryCollection)
+        .then(res => setData(res.docs.map(prod => ({id: prod.id, ...prod.data()}))))
+        .catch(err => console.log(err))
+      }, []);
+
+      console.log(data)
+
     return (
     <div className={openCart===true ? 'cartOpen' : 'cartClose'}>
         <div className='cart_close'>
@@ -15,8 +29,11 @@ const CartContent = ({openCart, handleCloseCart}: CartContentProps) => {
         <div className='cart_shipping'>
           <p>$123 más para tener envío gratis</p>
         </div>
-        <div className='cart_empty'>
+        {/* <div className='cart_empty'>
           <p>Tu carrito está vacío.</p>
+        </div> */}
+        <div className='cart_products'>
+        <CartItem products={data}/>
         </div>
     </div>
   )
--- src/Components/Navbar/CartContent/CartItem/CartItem.tsx
+++ src/Components/Navbar/CartContent/CartItem/CartItem.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './CartItem.scss';
 
-const CartItem = () => {
+const CartItem = ({products}:any) => {
   return (
-    <div>CartItem</div>
-  )
+    <>
+    {products.map((product:any) =>
+    <div className='cartItem'>
+      <div className='cartItem_image'>
+        <img  className='cartItem_image_img' src={product.images[0]}/>
+      </div>
+      <div className='cartItem_content'>
+        <div className='cartItem_content_div'>
+          <h3 className='cartItem_content_div_h3'>{product.name}</h3>
+          <p className='cartItem_content_div_p'>M</p>
+          <p className='cartItem_content_div_p'>1 x ${product.price}</p>
+        </div>
+        <div className='cartItem_content_delete'>
+          <p className='cartItem_content_delete_p'>Eliminar</p>
+        </div>
+      </div>
+    </div>)}
+    </>
+  );
 }
 
-export default CartItem
+export default CartItem;
