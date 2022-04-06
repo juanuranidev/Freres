fce18f6eaeb24ccbdb60badb494780666e1337bc
Juan Urani <juanuranidev@gmail.com>
2022-04-06 00:31:11 -0300

    updates

--- src/Components/Pages/Checkout/Form/Form.tsx
+++ src/Components/Pages/Checkout/Form/Form.tsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import { useForm, SubmitHandler  } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import MercadoPago from './MercadoPago/MercadoPago';
 import './Form.scss';
 
+interface IFormInputs {
+  "Nombre": string;
+  "Apellido": string;
+  "Email": string;
+  "Dirección": string;
+  "Localidad/Ciudad": string;
+  "Apartamento/habitación": string;
+  "Provincia": string;
+  "Código Postal": number;
+  "Teléfono": number;
+  "SuscribirseAlNewsletter": string;
+}
+
 const Form = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>();
+  const onSubmit: SubmitHandler<IFormInputs> = data => {
+    console.log(data)
+    alert("datos enviados")
+  }
+
   return (
-    <div className='form'>
-    
+    <div className="form">
+      <h2 className="form_h2">DETALLES DE LA FACTURACIÓN</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>  
+          <label>
+            <p>Nombre</p>
+            <input type="text" placeholder="Nombre" {...register("Nombre", {required: true, maxLength: 20})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+            <p>Apellido</p>
+            <input type="text" placeholder="Apellido" {...register("Apellido", {required: true, maxLength: 100})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+            <p>Email</p>
+            <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+            <p>Dirección</p>
+            <input type="text" placeholder="Dirección" {...register("Dirección", {required: true})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+            <p>Apartamento, habitación, etc. (opcional)</p>
+            <input type="text" placeholder="Apartamento, habitación, etc. (opcional)" {...register("Apartamento/habitación", {required: false})} />
+          </label>
+        </div>
+        <div>
+          <label>
+            <p>Localidad/Ciudad</p>
+            <input type="text" placeholder="Localidad/Ciudad" {...register("Localidad/Ciudad", {required: true})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+          <p>Provincia</p>
+            <input type="text" placeholder="Provincia" {...register("Provincia", {required: true})} /> 
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+            <p>Código Postal </p>
+            <input type="number" placeholder="Código Postal" {...register("Código Postal", {required: true, min: 1, maxLength: 10})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label>
+            <p>Teléfono</p>
+            <input type="number" placeholder="Teléfono" {...register("Teléfono", {required: true, min: 1, maxLength: 20})} />
+            {errors.Nombre?.type === 'required' && <p className='required'>Campo requerido</p>}
+          </label>
+          <label className='button'>
+            <button type="submit">REALIZAR PEDIDO</button>
+          </label>
+        </div>   
+      </form>    
+      <Link className="form_a" to="/">
+        <p className="form_a_p">VOLVER AL INICIO</p>
+      </Link>
+      <MercadoPago/>
     </div>
   );
 }
---
+++ src/Components/Pages/Checkout/Form/MercadoPago/MercadoPago.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './MercadoPago.scss';
+
+const MercadoPago = () => {
+  return (
+    <div className='mercadoPago'>
+        <div className='cards'>
+            <h3 className='cards_h3'>TARJETAS DE CRÉDITO</h3>
+            <div className='cards_div'>
+                <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/770edaa0-5dc7-11ec-a13d-73e40a9e9500-xl@2x.png"/>
+                <img className='cards_div_img' src="https://www.mercadopago.com/org-img/MP3/API/logos/visa.gif"/>
+                <img className='cards_div_img' src="https://www.mercadopago.com/org-img/MP3/API/logos/cabal.gif"/>
+                <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/33ea00e0-571a-11e8-8364-bff51f08d440-xl@2x.png"/>
+                <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/751ea930-571a-11e8-9a2d-4b2bd7b1bf77-xl@2x.png"/>
+                <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/d7e55980-f3be-11eb-8e0d-6f4af49bf82e-xl@2x.png"/>
+                <img className='cards_div_img' src="https://www.mercadopago.com/org-img/MP3/API/logos/master.gif"/>
+                <img className='cards_div_img' src="https://www.mercadopago.com/org-img/MP3/API/logos/amex.gif"/>
+            </div>
+        </div>
+        <div className='otherTypeOfCards'>
+            <div>
+                <h3>TARJETAS DE DÉBITO</h3>
+                <div>
+                    <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/ce454480-445f-11eb-bf78-3b1ee7bf744c-xl@2x.png"/>
+                    <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/d589be70-eb86-11e9-b9a8-097ac027487d-xl@2x.png"/>
+                    <img className='cards_div_img' src="https://http2.mlstatic.com/storage/logos-api-admin/0daa1670-5c81-11ec-ae75-df2bef173be2-xl@2x.png"/>
+                    <img className='cards_div_img cabal' src="https://http2.mlstatic.com/storage/logos-api-admin/c9f71470-6f07-11ec-9b23-071a218bbe35-xl@2x.png"/>
+                </div>
+            </div>
+            <div>
+                <h3>PAGOS EN EFECTIVO</h3>
+                <div>
+                    <img className='cards_div_img' src="https://www.mercadopago.com/org-img/MP3/API/logos/pagofacil.gif"/>
+                    <img className='cards_div_img' src="https://www.mercadopago.com/org-img/MP3/API/logos/rapipago.gif"/>
+                </div>
+            </div>
+        </div>
+        <div className='payment'>
+            <img src="https://freres.ar/wp-content/plugins/woocommerce-mercadopago/assets/images/redirect_checkout.png"/>
+            <h3>TE LLEVAMOS A NUESTRO SITIO PARA REALIZAR EL PAGO</h3>
+        </div>
+    </div>
+  );
+}
+
+export default MercadoPago;
