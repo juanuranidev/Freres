7d7ed59a6a9491b84a3bf35506857e1528a0943e
Juan Urani <juanuranidev@gmail.com>
2022-05-05 22:22:34 -0300

    updates

--- src/Components/Pages/Shop/Shop.tsx
+++ src/Components/Pages/Shop/Shop.tsx
@@ -1,16 +1,31 @@
 import React, {useState, useEffect } from 'react';
 import { getFirestore, query, collection, where, getDocs } from 'firebase/firestore';
+import { Link, useParams, useMatch, useResolvedPath } from 'react-router-dom';
+import type { LinkProps } from "react-router-dom";
 import { ProductModel } from '../../Context/CartContext';
-import { useParams } from 'react-router-dom';
-import Loader from '../../Loader/Loader';
 import ProductList from '../../ProductList/ProductList';
+import Loader from '../../Loader/Loader';
 import './Shop.scss';
+
+function CustomLink({ children, to }: LinkProps) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <Link
+      to={to}
+      className='shop_categories_a'
+      style={{ fontWeight : match ? '700' : '400' }}>
+      {children}
+    </Link>
+  );
+}
 
 const Shop = () => {
   const [data, setData] = useState<ProductModel[]>([]);
   const [loader, setLoader] = useState<boolean>(true);
   const {idCategory} = useParams();
-  
+
   useEffect(() => {
     setLoader(true)
     
@@ -29,6 +44,19 @@ const Shop = () => {
 
   return (
     <section className='shop'>
+      <div className='shop_categories'>
+        <CustomLink to="/shop/camperasybuzos">CAMPERAS Y BUZOS</CustomLink>
+        <p className='shop_categories_p'>/</p>
+        <CustomLink to="/shop/remeras">REMERAS</CustomLink>
+        <p className='shop_categories_p'>/</p>
+        <CustomLink to="/shop/pantalones">PANTALONES Y SHORTS</CustomLink>
+        <p className='shop_categories_p'>/</p>
+        <CustomLink to="/shop/calzado">CALZADO</CustomLink>
+        <p className='shop_categories_p'>/</p>
+        <CustomLink to="/shop/accesorios">ACCESORIOS</CustomLink>
+        <p className='shop_categories_p'>/</p>
+        <CustomLink to="/shop/all">TODOS LOS PRODUCTOS</CustomLink>
+      </div>
     {loader===true
     ? <Loader/>
     : <div className='shop_div'>
