b54efa149d98184e6e0043943f25ba237164a11d
Juan Urani <juanuranidev@gmail.com>
2022-09-27 23:16:54 -0300

    Working on NotFound

--- src/Components/Pages/NotFound/NotFound.tsx
+++ src/Components/Pages/NotFound/NotFound.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import SecondaryButton from '../../Buttons/SecondaryButton/SecondaryButton';
 import './NotFound.scss';
 
 const NotFound = () => {
   return (
     <section className='notFound'>
-      <h2>PAGINA NO ENCONTRADA</h2>
+      <div className='notFound_div'>
+        <h2 className='notFound_div_h2'>404</h2>
+        <h2 className='notFound_div_h2'>LO SIENTO FRERER, PAGINA NO ENCONTRADA.</h2>
+        <SecondaryButton text="VOLVER AL INICIO" link="/" onClick={() => null} />
+      </div>
     </section>
   );
 };
