e7c617235042596c73566f3a460c15f41a4ef95f
Juan Urani <juanuranidev@gmail.com>
2022-11-20 23:59:49 -0300

    Working on EssentialOutfitDetail

--- src/Components/Pages/EssentialOutfitDetail/EssentialOutfitDetail.tsx
+++ src/Components/Pages/EssentialOutfitDetail/EssentialOutfitDetail.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { ProductModel } from '../../Context/CartContext';
 import Loader from '../../Loader/Loader';
 import ProductList from '../../ProductList/ProductList';
 import useGetProducts from '../../Hooks/useGetProducts';
 import TertiaryButton from '../../Buttons/TertiaryButton/TertiaryButton';
+import ModalBackground from '../../Modals/ModalBackground/ModalBackground';
 import ModalSelectSizes from '../../Modals/ModalSelectSizes/ModalSelectSizes';
 import './EssentialOutfitDetail.scss';
-import ModalBackground from '../../Modals/ModalBackground/ModalBackground';
 
 const EssentialOutfitDetail = () => {
   const { idOutfit } = useParams();
   const {loader, products} = useGetProducts('essential_outfit', '==', idOutfit)
-  const [modalSelectSizes, setModalSelectSizes] = useState(false)
+  const [modalSelectSizes, setModalSelectSizes] = useState<boolean>(false)
+  const [modalProducts, setModalProducts] = useState<ProductModel[]>([])
+
+  const handleOpenModalSizes = () => {
+    setModalProducts(products)
+    setModalSelectSizes(true)
+  }
+
+  const handleCloseModalSizes = () => {
+    setModalProducts([])
+    setModalSelectSizes(false)
+  }
 
   if(loader) return <Loader/>
   
@@ -28,7 +40,7 @@ const EssentialOutfitDetail = () => {
         <div className='essentialOutfitDetail_content_div'>
           <h1 className='essentialOutfitDetail_content_h1'>{idOutfit} Outfit</h1>
           <img src={products[0].essential_outfit_image} className='essentialOutfitDetail_content_img' alt="Imagen de producto"/>
-          <TertiaryButton text="AGREGAR AL CARRITO" onClick={() => setModalSelectSizes(true)}/>
+          <TertiaryButton text="AGREGAR AL CARRITO" onClick={handleOpenModalSizes}/>
         </div>
         <div className='essentialOutfitDetail_content_products'>
           <ProductList products={products}/>
@@ -37,11 +49,11 @@ const EssentialOutfitDetail = () => {
       <ModalSelectSizes 
         open={modalSelectSizes}
         modalProducts={products}
-        close={() => setModalSelectSizes(false)}
+        close={handleCloseModalSizes}
       />
       <ModalBackground 
         open={modalSelectSizes}
-        close={() => setModalSelectSizes(false)}
+        close={handleCloseModalSizes}
       />
     </section>
   );
