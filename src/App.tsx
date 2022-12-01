7e679cbc89cee77cc29199906331327a9ec4b32f
Juan Urani <juanuranidev@gmail.com>
2022-11-30 23:38:15 -0300

    Added lazy loading to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,51 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { NewsletterContextProvider } from './Components/Context/NewsletterContext';
-import { CartContextProvider } from './Components/Context/CartContext';
-import { getFirestoreApp } from './Services/Firebase/DbConfig';
-import Main from './Components/Pages/Main/Main';
-import Shop from './Components/Pages/Shop/Shop';
-import About from './Components/Pages/About/About';
-import Footer from './Components/Footer/Footer';
-import Navbar from './Components/Navbar/Navbar';
-import Checkout from './Components/Pages/Checkout/Checkout';
-import NotFound from './Components/Pages/NotFound/NotFound';
-import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
-import ProductFinder from './Components/Pages/ProductDetail/ProductFinder';
-import SecretDiscount from './Components/Pages/SecretDiscount/SecretDiscount';
-import BuildYourOutfit from './Components/Pages/BuildYourOutfit/BuildYourOutfit';
-import EssentialOutfits from './Components/Pages/EssentialOutfits/EssentialOutfits';
-import EssentialOutfitDetail from './Components/Pages/EssentialOutfitDetail/EssentialOutfitDetail';
-import './Assets/Styles/Base.scss';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NewsletterContextProvider } from "./Components/Context/NewsletterContext";
+import { CartContextProvider } from "./Components/Context/CartContext";
+import { getFirestoreApp } from "./Services/Firebase/DbConfig";
+import "./Assets/Styles/Base.scss";
 
-getFirestoreApp()
+const Main = React.lazy(() => import("./Components/Pages/Main/Main"));
+const Shop = React.lazy(() => import("./Components/Pages/Shop/Shop"));
+const About = React.lazy(() => import("./Components/Pages/About/About"));
+const Footer = React.lazy(() => import("./Components/Footer/Footer"));
+const Navbar = React.lazy(() => import("./Components/Navbar/Navbar"));
+const Checkout = React.lazy(() => import("./Components/Pages/Checkout/Checkout"));
+const NotFound = React.lazy(() => import("./Components/Pages/NotFound/NotFound"));
+const ScrollToTop = React.lazy(() => import("./Components/ScrollToTop/ScrollToTop"));
+const ProductFinder = React.lazy(() => import("./Components/Pages/ProductDetail/ProductFinder"));
+const SecretDiscount = React.lazy(() => import("./Components/Pages/SecretDiscount/SecretDiscount"));
+const BuildYourOutfit = React.lazy(() => import("./Components/Pages/BuildYourOutfit/BuildYourOutfit"));
+const EssentialOutfits = React.lazy(() => import("./Components/Pages/EssentialOutfits/EssentialOutfits"));
+const EssentialOutfitDetail = React.lazy(() => import("./Components/Pages/EssentialOutfitDetail/EssentialOutfitDetail"));
+
+getFirestoreApp();
 
 function App() {
   return (
-    <CartContextProvider>
-      <NewsletterContextProvider>
-        <BrowserRouter>
-          <ScrollToTop/>
-          <Navbar/>
-          <Routes>
-            <Route path='/' element={<Main/>}/>
-            <Route path='/shop/:idCategory' element={<Shop/>}/>
-            <Route path='/essential_outfits' element={<EssentialOutfits/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/product/:idProduct' element={<ProductFinder/>}/>
-            <Route path='/outfit/:idOutfit' element={<EssentialOutfitDetail/>}/>
-            <Route path='/build-your-outfit' element={<BuildYourOutfit/>}/>
-            <Route path='/secret' element={<SecretDiscount/>}/>
-            <Route path='/checkout' element={<Checkout/>}/>
-            <Route path='*' element={<NotFound/>}/>
-          </Routes>
-          <Footer/>
-        </BrowserRouter>
-      </NewsletterContextProvider>
-    </CartContextProvider>
+    <Suspense fallback={<div>Loading... </div>}>
+      <CartContextProvider>
+        <NewsletterContextProvider>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/shop/:idCategory" element={<Shop />} />
+              <Route path="/essential_outfits" element={<EssentialOutfits />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/product/:idProduct" element={<ProductFinder />} />
+              <Route path="/outfit/:idOutfit" element={<EssentialOutfitDetail />}/>
+              <Route path="/build-your-outfit" element={<BuildYourOutfit />} />
+              <Route path="/secret" element={<SecretDiscount />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </NewsletterContextProvider>
+      </CartContextProvider>
+    </Suspense>
   );
 }
 
