28e0455d2440b810c419221c01341bd52e955352
juanuranidev <juanuranidev@gmail.com>
2023-05-07 22:56:38 -0300

    Fixing Git error

---
+++ src/Services/newsletter.js
@@ -0,0 +1,40 @@
+import {
+  query,
+  where,
+  addDoc,
+  getDocs,
+  collection,
+  getFirestore,
+} from "firebase/firestore";
+
+const dataBase = getFirestore();
+
+export const verifyEmail = async (userEmail) => {
+  try {
+    let emailObject = { email: userEmail };
+
+    const emailCollection = collection(dataBase, "newsletter");
+    const queryCollection = query(
+      emailCollection,
+      where("email", "==", emailObject.email)
+    );
+
+    const response = await getDocs(queryCollection);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const uploadEmail = async (userEmail) => {
+  try {
+    let emailObject = { email: userEmail };
+
+    const emailCollection = collection(dataBase, "newsletter");
+
+    const response = await addDoc(emailCollection, emailObject);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
