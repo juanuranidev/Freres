9fd506b31368692b1dcc615f402cc5a7365d9dbd
juanuranidev <juanuranidev@gmail.com>
2023-05-26 23:48:06 -0300

    Added new products services and utils

--- src/Services/products.js
+++ src/Services/products.js
@@ -1,4 +1,12 @@
-import { getDocs, collection, getFirestore } from "firebase/firestore";
+import {
+  doc,
+  where,
+  query,
+  getDoc,
+  getDocs,
+  collection,
+  getFirestore,
+} from "firebase/firestore";
 
 const dataBase = getFirestore();
 
@@ -7,6 +15,45 @@ export const handleGetAllProducts = async () => {
     const emailCollection = collection(dataBase, "products");
 
     const response = await getDocs(emailCollection);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const handleGetProductsByCategory = async (category) => {
+  try {
+    const categoryCollection = query(
+      collection(dataBase, "products"),
+      where("category", "==", category)
+    );
+
+    const response = await getDocs(categoryCollection);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const handleGetProductById = async (productId) => {
+  try {
+    const productQuery = doc(dataBase, "products", productId);
+
+    const response = await getDoc(productQuery);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const handleGetTopSellerProducts = async () => {
+  try {
+    const categoryCollection = query(
+      collection(dataBase, "products"),
+      where("top_seller", "==", true)
+    );
+
+    const response = await getDocs(categoryCollection);
     return response;
   } catch (error) {
     return error;
--- src/Utils/products.js
+++ src/Utils/products.js
@@ -1,3 +1,10 @@
-export const mapProducts = (products) => {
+export const formatProducts = (products) => {
   return products.map((prod) => ({ id: prod.id, ...prod.data() }));
+};
+
+export const handleFormatDescription = (description) => {
+  let productDescription = description.split(".");
+  productDescription.pop();
+
+  return productDescription;
 };
