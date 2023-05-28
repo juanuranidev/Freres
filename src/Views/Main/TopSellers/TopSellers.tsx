9aa3925b307ad2732e91e426863f12ff4e7c1385
juanuranidev <juanuranidev@gmail.com>
2023-05-27 22:31:01 -0300

    Refactorized top sellers view

--- src/Views/Main/TopSellers/TopSellers.tsx
+++ src/Views/Main/TopSellers/TopSellers.tsx
@@ -1,24 +1,42 @@
-import React from "react";
-import Loader from "../../../Components/Loader/Loader";
-import ProductList from "../../../Components/ProductList/ProductList";
+import React, { useState, useEffect } from "react";
+import { handleGetTopSellerProducts } from "../../../Services/products";
+import { formatProducts } from "../../../Utils/products";
+import { ProductModel } from "../../../Models/product.model";
 import PrimaryButton from "../../../Components/Buttons/PrimaryButton/PrimaryButton";
-import useGetCustomProducts from "../../../Hooks/useGetCustomProducts";
+import ProductList from "../../../Components/ProductList/ProductList";
+import Loader from "../../../Components/Loader/Loader";
 import "./TopSellers.scss";
 
 const TopSellers = () => {
-  const { loader, products } = useGetCustomProducts("top_seller", "==", true);
+  const [loader, setLoader] = useState<boolean>(false);
+  const [products, setProducts] = useState<ProductModel[]>([]);
+
+  const handleGetProducts = async () => {
+    setLoader(true);
+    try {
+      const response: any = await handleGetTopSellerProducts();
+      setProducts(formatProducts(response.docs));
+    } catch (error) {
+      console.log(error);
+      setLoader(false);
+    }
+    setLoader(false);
+  };
+
+  useEffect(() => {
+    handleGetProducts();
+  }, []);
 
   if (loader) return <Loader />;
-
-  if (!products.length) return null;
+  if (!products!.length) return null;
 
   return (
-    <section className="topSellers">
-      <h2 className="topSellers_h2">TOP SELLERS</h2>
-      <div className="topSellers_div">
-        <ProductList products={products} />
+    <section className="top_sellers">
+      <h2 className="top_sellers_h2">TOP SELLERS</h2>
+      <div className="top_sellers_div">
+        <ProductList products={products!} />
       </div>
-      <div className="topSellers_button">
+      <div className="top_sellers_button">
         <PrimaryButton link="/shop/all" text="TODOS LOS PRODUCTOS" />
       </div>
     </section>
