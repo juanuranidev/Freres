ced12627cb145019947e9cf9a48619bcdb2dd25d
juanuranidev <juanuranidev@gmail.com>
2023-05-28 23:34:25 -0300

    Refactorized main view

--- src/Views/Main/Slider/Slider.tsx
+++ src/Views/Main/Slider/Slider.tsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
-import { Images } from './ImportImages';
-import './Slider.scss';
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { Images } from "./ImportImages";
+import "./Slider.scss";
 
 const Slider = () => {
+  const carousel: any = useRef();
+
   const [width, setWidth] = useState<number>(0);
-  const carousel:any = useRef();
 
   useEffect(() => {
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth + 10);
   }, []);
 
   return (
-    <section className='slider'>
+    <section className="slider">
       <motion.div
-        className='slider_container' 
-        drag='x' 
-        ref={carousel} 
-        whileTap={{cursor: "grabbing"}}
-        dragConstraints={{right: 0, left: -width}}> 
-          {Images.map((image:any, index:number) => 
-            <motion.div className='slider_container_div' key={index}>
-              <img src={image} className='slider_container_div_img' alt="Imágen de modelo Freres"/>
-            </motion.div>)}
+        className="slider_container"
+        drag="x"
+        ref={carousel}
+        whileTap={{ cursor: "grabbing" }}
+        dragConstraints={{ right: 0, left: -width }}
+      >
+        {Images.map((image: string, index: number) => (
+          <motion.div className="slider_container_div" key={index}>
+            <img
+              src={image}
+              className="slider_container_div_img"
+              alt="Imágen de modelo Freres"
+            />
+          </motion.div>
+        ))}
       </motion.div>
     </section>
   );
-}
+};
 
 export default Slider;
-
