65895718d145f4cf9d9da1402e1a3d5e8f4ad5a8
juanuranidev <juanuranidev@gmail.com>
2023-05-29 23:28:49 -0300

    Refactorized modal select sizes

--- src/Components/Modals/ModalSelectSizes/ModalSelectSizes.tsx
+++ src/Components/Modals/ModalSelectSizes/ModalSelectSizes.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
-import { CartContext, ProductModel } from "../../../Context/CartContext";
+import { ProductModel } from "../../../Models/product.model";
+import { CartContext } from "../../../Context/CartContext";
 import ProductSizes from "../../../Views/ProductDetail/ProductSizes/ProductSizes";
 import TertiaryButton from "../../Buttons/TertiaryButton/TertiaryButton";
 import "./ModalSelectSizes.scss";
@@ -15,55 +16,40 @@ const ModalSelectSizes = ({
   close,
   modalProducts,
 }: ModalSelectSizesProps) => {
+  const { addToCart } = useContext(CartContext);
+
   const [size, setSize] = useState<string>("");
   const [modalProduct, setModalProduct] = useState<number>(0);
   const [productsWithSizes, setProductWithSizes] = useState<ProductModel[]>([]);
-
-  const { addToCart } = useContext(CartContext);
-
-  useEffect(() => {
-    handleGetSize();
-  }, [modalProduct, size]);
 
   const handleNext = () => setModalProduct((prev) => prev + 1);
   const handlePrevious = () => setModalProduct((prev) => prev - 1);
 
   const handleGetSize = () => {
-    const exist = productsWithSizes.find(
-      (product: ProductModel) => product.id === modalProducts[modalProduct].id
-    );
+    const product = modalProducts[modalProduct];
+    const exist = productsWithSizes.find((p) => p.id === product.id);
 
-    if (exist?.size !== "") {
-      setSize(exist?.size || "");
+    if (exist?.size) {
+      setSize(exist.size);
     } else {
       setSize("");
-      setProductWithSizes(
-        productsWithSizes.filter(
-          (product: ProductModel) =>
-            product.id !== modalProducts[modalProduct].id
-        )
-      );
+      setProductWithSizes(productsWithSizes.filter((p) => p.id !== product.id));
     }
   };
 
   const handleSetSize = (size: string) => {
-    const hasASize = productsWithSizes.some(
-      (product: ProductModel) => product.id === modalProducts[modalProduct].id
-    );
+    const product = modalProducts[modalProduct];
+    const hasASize = productsWithSizes.some((p) => p.id === product.id);
     setSize(size);
 
     if (hasASize) {
-      return setProductWithSizes(
-        productsWithSizes.map((product: ProductModel) =>
-          product.id === modalProducts[modalProduct].id
-            ? { ...product, size: size }
-            : product
-        )
+      setProductWithSizes((prevProducts) =>
+        prevProducts.map((p) => (p.id === product.id ? { ...p, size } : p))
       );
     } else {
-      return setProductWithSizes([
-        ...productsWithSizes,
-        { ...modalProducts[modalProduct], size },
+      setProductWithSizes((prevProducts) => [
+        ...prevProducts,
+        { ...product, size },
       ]);
     }
   };
@@ -75,42 +61,51 @@ const ModalSelectSizes = ({
     close();
   };
 
+  useEffect(() => {
+    handleGetSize();
+  }, [modalProduct, size]);
+
   return (
-    <div className={`modalSelectSizes ${open && "modalSelectSizes_visible"} `}>
-      <div className="modalSelectSizes_close">
-        <p className="modalSelectSizes_close_p" onClick={close}>
+    <div
+      className={`modal_select_sizes ${open && "modal_select_sizes_visible"} `}
+    >
+      <div className="modal_select_sizes_close">
+        <p className="modal_select_sizes_close_p" onClick={close}>
           CERRAR
         </p>
       </div>
-      <h2 className="modalSelectSizes_h2">Selecciona el talle para</h2>
-      <h2 className="modalSelectSizes_h2">
-        {modalProducts[modalProduct].name}
+      <h2 className="modal_select_sizes_h2">Selecciona el talle para</h2>
+      <h2 className="modal_select_sizes_h2">
+        {modalProducts[modalProduct]?.name}
       </h2>
       <img
-        src={modalProducts[modalProduct].images[0]}
-        className="modalSelectSizes_img"
+        src={modalProducts[modalProduct]?.images[0]}
+        className="modal_select_sizes_img"
       />
       <ProductSizes
-        sizeType={modalProducts[modalProduct].format_of_size_chart}
+        sizeType={modalProducts[modalProduct]?.format_of_size_chart}
         size={size}
         setSize={handleSetSize}
       />
-      <div className="modalSelectSizes_div">
+      <div className="modal_select_sizes_div">
         <div
-          className={`modalSelectSizes_div_actions${
+          className={`modal_select_sizes_div_actions${
             modalProduct === 0 ? " first" : ""
           }`}
         >
           {modalProduct !== 0 && (
             <p
               onClick={handlePrevious}
-              className="modalSelectSizes_div_actions_p"
+              className="modal_select_sizes_div_actions_p"
             >
               ANTERIOR
             </p>
           )}
           {modalProduct !== modalProducts.length - 1 && (
-            <p onClick={handleNext} className="modalSelectSizes_div_actions_p">
+            <p
+              onClick={handleNext}
+              className="modal_select_sizes_div_actions_p"
+            >
               SIGUIENTE
             </p>
           )}
