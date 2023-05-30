5bd5227dacc8e8234fc062be60a501c921749f01
juanuranidev <juanuranidev@gmail.com>
2023-05-29 23:27:41 -0300

    Refactorized socials component

--- src/Components/Socials/Socials.tsx
+++ src/Components/Socials/Socials.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import './Socials.scss';
+import React from "react";
+import "./Socials.scss";
 
 const Socials = () => {
   return (
-    <div className='socials'>
-      <a className='socials_a' href='https://wa.link/1bbe3z' target='_blank' rel="noreferrer"><span className='socials_a_span fab fa-whatsapp fa-3x' /></a>
-      <a className='socials_a' href='https://www.instagram.com/freres/' target='_blank' rel="noreferrer"><span className='socials_a_span fab fa-instagram fa-3x' /></a>
-      <a className='socials_a' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel="noreferrer"><span className='socials_a_span fab fa-youtube fa-3x' /></a>
+    <div className="socials">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="socials_a"
+        href="https://wa.link/1bbe3z"
+      >
+        <span className="socials_a_span fab fa-whatsapp fa-3x" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="socials_a"
+        href="https://www.instagram.com/freres/"
+      >
+        <span className="socials_a_span fab fa-instagram fa-3x" />
+      </a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="socials_a"
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      >
+        <span className="socials_a_span fab fa-youtube fa-3x" />
+      </a>
     </div>
   );
-}
+};
 
 export default Socials;
