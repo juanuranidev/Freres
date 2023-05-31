2fe22acd6cb8bb855407a424bd397c9958359a23
juanuranidev <juanuranidev@gmail.com>
2023-05-30 23:57:48 -0300

    Refactorized primary button component

--- src/Components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ src/Components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './PrimaryButton.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./PrimaryButton.scss";
 
 interface PrimaryButtonProps {
   link: string;
   text: string;
 }
 
-const PrimaryButton = ({link, text}: PrimaryButtonProps) => {
+const PrimaryButton = ({ link, text }: PrimaryButtonProps) => {
   return (
     <Link to={link}>
-      <button className='primaryButton'>{text}</button>
+      <button className="primary_button">{text}</button>
     </Link>
   );
-}
+};
 
 export default PrimaryButton;
